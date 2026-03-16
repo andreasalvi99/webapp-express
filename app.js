1e3f109a20f5e6dbef5179af682211f659980691
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-03-16 15:44:58 +0100

    index route done

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const port = 3000;
 const url = "http://localhost:";
 const connection = require("./database/movies");
 
+//^ MIDDLEWARES
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const filmsSQL = `SELECT * FROM movies.movies`;
+  connection.query(filmsSQL, (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    res.json({
+      success: true,
+      results: results,
+    });
+  });
+
+  //   res.json({
+  //     success: true,
+  //     results: results,
+  //   });
+});
+
 app.listen(port, () => {
   console.log(`Server online on ${url}${port}`);
 });
